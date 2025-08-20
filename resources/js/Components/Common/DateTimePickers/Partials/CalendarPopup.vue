504820556ed890936aebf69814de66e276170895
Jersey276 <tristan-lefevre@hotmail.fr>
2025-08-21 00:34:25 +0200

    replace inputs by calendar on CalenderPopup

--- resources/js/Components/Common/DateTimePickers/Partials/CalendarPopup.vue
+++ resources/js/Components/Common/DateTimePickers/Partials/CalendarPopup.vue
@@ -1,76 +1,122 @@
 <script setup>
-import moment from 'moment';
-import { ref, watch } from 'vue';
+import { ref, watch, computed } from "vue";
+import moment from "moment";
 
-const emit = defineEmits(["change"]);
+const emit = defineEmits(['change']);
 
 const props = defineProps({
-  show: Boolean,
-  value: {
-    type: Object,
-    validator: moment.isMoment,
-  },
-  withDate: Boolean,
-  withTime: Boolean,
-  isClearable: Boolean,
-});
+    show: {
+        type: Boolean,
+        default: false
+    },
+    modelValue: {
+        type: Object,
+        validator: moment.isMoment,
+    },
+    withDate: {
+        type: Boolean,
+        default: false
+    },
+    withTime: {
+        type: Boolean,
+        default: false
+    }
+    
+})
 
-const selected = ref(props.value ? moment(props.value) : moment());
+const today = moment();
+const selected = ref(props.modelValue && moment.isMoment(props.modelValue) ? props.modelValue.clone() : today.clone());
+const currentMonth = ref(selected.value.clone().startOf('month'));
 
-watch(() => props.show, (show) => {
-    if (show) {
-        selected.value = props.value ? moment(props.value) : moment();
+const time = ref(selected.value.format('HH:mm'));
+
+console.log(currentMonth);
+
+watch(() => props.value, (newVal) => {
+    if (newVal && moment.isMoment(newVal)) {
+        selected.value = newVal.clone();
+        currentMonth.value = selected.value.clone().startOf('month');
+        time.value = selected.value.time;
     }
 });
 
-const data = {
-	date: selected.value.format('YYYY-MM-DD'),
-	time: selected.value.format('HH:mm'),
-	dateTime: selected.value.format('YYYY-MM-DDTHH:mm'),
+const daysInMonth = computed(() => {
+    return currentMonth.value.daysInMonth();
+});
+const firstDayOfWeek = computed(() => {
+    return currentMonth.value.day();
+});
+
+const weeks = computed(() => {
+    const weeks = [];
+    const days = Array.from({ length: daysInMonth.value }, (_, i) => {
+        return currentMonth.value.clone().date(i + 1);
+    });
+    while (days.length) {
+        weeks.push(days.splice(0, 7));
+    }
+    return weeks;
+});
+
+function selectDate(day) {
+    if (!day) return;
+    if (props.withDate && props.withTime) {
+        const [hour, minute] = time.value.split(':');
+        selected.value = day.clone().hour(Number(hour)).minute(Number(minute));
+        emit('change', selected.value.clone());
+    }
+    else if (props.withDate) {
+        selected.value = day.clone();
+        emit('change', selected.value.clone());
+    }
 }
 
-const onDateChange = (e) => {
-  const date = e.target.value;
-  selected.value = moment(date);
-  emit("change", selected.value.clone());
-}
-const onDateTimeChange = (e) => {
-  const datetime = e.target.value;
-  selected.value = moment(datetime);
-  emit("change", selected.value.clone());
-}
-const onTimeChange = (e) => {
-  const time = e.target.value;
-  selected.value = moment(time);
-  emit("change", selected.value.clone());
+function selectTime() {
+    if (props.withTime && !props.withDate) {
+        const [hour, minute] = time.value.split(':');
+        selected.value = today.clone().hour(Number(hour)).minute(Number(minute));
+        emit('change', selected.value.clone());
+    }
 }
 
-const clearValue = (e) => {
-  selected.value = moment();
-  emit("change", null);
+function prevMonth() {
+    if (currentMonth.value) {
+        currentMonth.value = currentMonth.value.clone().subtract(1, 'month');
+    }
 }
-
+function nextMonth() {
+    if (currentMonth.value) {
+        currentMonth.value = currentMonth.value.clone().add(1, 'month');
+    }
+}
 </script>
 <template>
-    <div v-show="show" class="bg-white border rounded shadow p-4 flex flex-row justify-between items-center">
-      <template v-if="withDate && withTime">
-        <input type="datetime-local" @change="onDateTimeChange" v-model="data.dateTime"/>
-      </template>
-      <template v-else-if="withTime">
-        <input type="time" @change="onTimeChange" v-model="data.time"/>
-      </template>
-      <template v-else-if="withDate">
-        <input type="date" @change="onDateChange" v-model="data.date"/>
-      </template>
-      <template v-if="selected.value !== null && isClearable">
-        <vueFeather
-          class="cursor-pointer ml-2 text-gray-700 hover:text-red-500 transition"
-          type="trash"
-          size="1.3rem"
-          @click="clearValue"
-        />
-      </template>
+    <div v-show="show" class="inline-calendar bg-white border rounded p-4 w-80">
+        <div v-if="withDate">
+            <div class="flex justify-between items-center mb-2 h-20">
+                <button class="self-start" @click="prevMonth">Previous</button>
+                <div class="font-bold">{{ currentMonth.format('MMMM YYYY') }}</div>
+                <button class="self-start" @click="nextMonth">Next</button>
+            </div>
+            <div>
+                <div class="grid grid-cols-7 text-center text-sm font-semibold mb-2">
+                    <span v-for="(day, index) in ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']" :key="index">{{ day }}</span>
+                </div>
+                <div class="grid grid-cols-7 gap-1">
+                    <span v-for="n in firstDayOfWeek" :key="`empty-${n}`" class="text-transparent">.</span>
+                    <span v-for="day in weeks.flat()" :key="day.format('YYYY-MM-DD')" 
+                        :class="{'bg-blue-500 text-white': selected.isSame(day, 'day'), 'cursor-pointer hover:bg-gray-200': true, ' text-center p-2 rounded': true}"
+                        @click="selectDate(day)">
+                        {{ day.date() }}
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div v-if="withTime">
+            <div class="flex w-full justify-around items-center">
+                <input type="time" v-model="time" class="border rounded p-1" />
+                <button v-if="!props.withDate && props.withTime" @click="selectTime" class="ml-2 bg-blue-500 text-white rounded px-2 py-1">Set Time</button>
+            </div>
+        </div>
     </div>
 </template>
-
-<style lang="postcss" scoped></style>
